681382d7de7c848a87d33a34355973805b143219
NellyNel520 <channelharris520@gmail.com>
2023-03-03 18:46:15 -0500

    setting up structure for public nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='text-white'>
+    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Tavern</h1>
+      <ul className='flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Register</li>
+        <li className='p-4'>SignIn</li>
+        <li className='p-4'>About</li>
+      </ul>
     </div>
   )
 }
