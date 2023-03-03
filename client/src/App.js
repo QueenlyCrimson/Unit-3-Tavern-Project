45e9ceaa06fe6051bda68f4b7a7457d9420068fe
NellyNel520 <channelharris520@gmail.com>
2023-03-03 14:18:58 -0500

    started nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from './componets/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      Tavern
+      <NavBar />
     </div>
   );
 }
