add059977c97f4c3e0a1b67066e900af708e2980
NellyNel520 <channelharris520@gmail.com>
2023-03-04 11:10:05 -0500

    created hero section and installed react typed package for typing effect

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className='msx-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col'>
+      <p className='text-[#d98345] font-bold p-2'>THE ULTIMATE SOCIAL MEDIA APP</p>
+      <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Connect with Tavern</h1>
+      <div>
+        <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Something for everyone whether you're a  </p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 
 const About = () => {
   return (
-    <div className='text-white'>About</div>
+    <div className='text-white'>
+      <h1 className='w-full text-3xl font-bold text-center'>About</h1>
+      <Hero />
+    </div>
   )
 }
 
