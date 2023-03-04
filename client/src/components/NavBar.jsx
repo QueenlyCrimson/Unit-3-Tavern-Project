0c08c1f37831d93e93621c69fd9c998f67726d9b
Chikodi Merenu <cmerenu@gmail.com>
2023-03-04 11:59:17 -0600

    save work

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+// import MakePost from './MakePost'
+import MakeProfile from '../pages/MakeProfile'
 
 const NavBar = () => {
   return (
     <div className='text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Tavern</h1>
+      <MakeProfile />
+      {/* <MakePost />s */}
     </div>
   )
 }
