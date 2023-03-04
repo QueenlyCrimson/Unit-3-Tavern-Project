20a4b7fdbc2e7c3b714bc4d4026203d2c5b02937
NellyNel520 <channelharris520@gmail.com>
2023-03-04 14:29:37 -0500

    setting up about us company info section

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4 text-black'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        {/* <img /> Tavern Logo*/}
+        <div>
+          <p>THE ULTIMATE SOCIAL MEDIA APP</p>
+          <h1>Connect with Like-Minded Individuals Across the World </h1>
+          <p>Whether you're a foodie, gamer, fitness enthusiast, or fashionista, Tavern has something for everyone. With its user-friendly interface, Tavern allows you to create a personalized profile that showcases your unique personality and interests.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Description
