d88386c3e59fc97a2a1c4446055f802c070a3405
NellyNel520 <channelharris520@gmail.com>
2023-03-04 12:54:20 -0500

    changed colors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <nav>
     <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#d98345]'>Tavern.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#e57626]'>Tavern.</h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Register</li>
@@ -24,7 +24,7 @@ const NavBar = () => {
 
 
       <div className={!nav ? 'fixed left-0 top-0 w-{60%} h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[100%]'}>
-      <h1 className='w-full text-3xl font-bold text-[#d98345] m-4'>Tavern.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#e57626] m-4'>Tavern.</h1>
 
         <ul className='uppercase p-4 '>
         <li className='p-4 border-b border-gray-600' border-b>Home</li>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className='text-white'>
       <h1 className='w-full text-3xl font-bold text-center'>About</h1>
-      <Hero />
+      
     </div>
   )
 }
