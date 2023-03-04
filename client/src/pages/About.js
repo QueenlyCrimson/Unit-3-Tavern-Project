ae4ad164006281ce3f9772a8c40721daab1ae365
Chikodi Merenu <cmerenu@gmail.com>
2023-03-04 15:02:45 -0600

    SignIn and Register

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero'
 
-
 const About = () => {
   return (
-    <div className='text-white'>
-      <h1 className='w-full text-3xl font-bold text-center'>About</h1>
-      
+    <div className="text-white">
+      <h1 className="w-full text-3xl font-bold text-center">About</h1>
+      <Hero />
     </div>
   )
 }
--- client/src/pages/MakeProfile.jsx
+++ client/src/pages/MakeProfile.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import MakePost from "../components/MakePost"
+// import MakePost from "../components/MakePost"
 
 const MakeProfile = () => {
   return (
@@ -8,8 +8,8 @@ const MakeProfile = () => {
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-base font-semibold leading-6 text-gray-900">Profile</h3>
-              <p className="mt-1 text-sm text-gray-600">
+              <h3 className="text-base font-semibold leading-6 text-[#e57626]">Profile</h3>
+              <p className="mt-1 text-sm text-white">
                 This information will be displayed publicly so be careful what you share.
               </p>
             </div>
@@ -56,7 +56,7 @@ const MakeProfile = () => {
                         <div className="flex text-sm text-gray-600">
                           <label
                             htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                            className="relative cursor-pointer rounded-md bg-white font-medium text-[#e57626] focus-within:outline-none focus-within:ring-2 focus-within:ring-[#c66c2c] focus-within:ring-offset-2 hover:text-[#e99253]"
                           >
                             <span>Upload a file</span>
                             <input id="file-upload" name="file-upload" type="file" className="sr-only" />
@@ -71,7 +71,7 @@ const MakeProfile = () => {
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
                     type="submit"
-                    className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    className="inline-flex justify-center rounded-md bg-[#e57626] py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-[#e99253] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                   >
                     Save
                   </button>
@@ -92,7 +92,10 @@ const MakeProfile = () => {
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-base font-semibold leading-6 text-gray-900">Personal Information</h3>
+              <h3 className="text-base font-semibold leading-6 text-[#e57626]">Personal Information</h3>
+              <p className="mt-1 text-sm text-white">
+                This information will be displayed publicly so be careful what you share.
+              </p>
             </div>
           </div>
           <div className="mt-5 md:col-span-2 md:mt-0">
@@ -109,7 +112,7 @@ const MakeProfile = () => {
                         name="name"
                         id="name"
                         autoComplete="given-name"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
                       />
                     </div>
 
@@ -122,7 +125,7 @@ const MakeProfile = () => {
                         name="userName"
                         id="userName"
                         autoComplete="family-name"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
 
@@ -135,7 +138,7 @@ const MakeProfile = () => {
                         name="email"
                         id="email"
                         autoComplete="email"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                     <div className="col-span-6">
@@ -147,7 +150,7 @@ const MakeProfile = () => {
                         name="password"
                         id="password"
                         autoComplete="password"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
@@ -155,7 +158,7 @@ const MakeProfile = () => {
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
                     type="submit"
-                    className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    className="inline-flex justify-center rounded-md bg-[#e57626] py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-[#e99253] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                   >
                     Save
                   </button>
