7410a812754f888133ff64a998ec1be52d0349d0
NellyNel520 <channelharris520@gmail.com>
2023-03-04 11:38:49 -0500

    added typed effect to hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import Typed from 'react-typed'
 
 const Hero = () => {
   return (
     <div>
-      <div className='msx-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col'>
+      <div className='max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col'>
       <p className='text-[#d98345] font-bold p-2'>THE ULTIMATE SOCIAL MEDIA APP</p>
       <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Connect with Tavern</h1>
       <div>
-        <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Something for everyone whether you're a  </p>
+        <p className='md:text-3xl sm:text-2xl text-xl font-bold'>For everyone whether you're a  </p>
+        <Typed strings={[
+          'Foodie',
+          'Gamer',
+          'Fitness Enthusiast', 
+          'Fashionista'
+        ]} 
+        typeSpeed={60}
+        backSpeed={20}
+        loop
+        />
       </div>
       </div>
     </div>
