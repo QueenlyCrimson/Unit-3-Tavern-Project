3bf6bc52cc82fb0832b570ddbe2b5df49055bcf9
Chikodi Merenu <cmerenu@gmail.com>
2023-03-07 10:06:22 -0600

    remove errors

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -10,7 +10,7 @@ const Description = () => {
           <p className='text-[#e57626] font-bold'>THE ULTIMATE SOCIAL MEDIA APP</p>
           <h1 className='md:text-4xl sm:text-3xl text-3xl font-bold py-2'>Connect with Like-Minded Individuals Across the World </h1>
           <p>Whether you're a foodie, gamer, fitness enthusiast, or fashionista, Tavern has something for everyone. With its user-friendly interface, Tavern allows you to create a personalized profile that showcases your unique personality and interests.</p>
-          <button className='bg-[#e57626] w-[200px] rounded-md my-6 mx-auto md:mx-0 py-3 text-black'>Register Now!</button>
+          <button ahref="/makeProfile" className='bg-[#e57626] w-[200px] rounded-md my-6 mx-auto md:mx-0 py-3 text-black'>Register Now!</button>
         </div>
       </div>
     </div>
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -112,7 +112,6 @@ const SignIn = (props) => {
               <button
                 type="submit"
                 onClick={handleSubmit}
-                a href='/feed'
                 className="group relative flex w-full justify-center rounded-md bg-orange-500 py-2 px-3 text-sm font-semibold text-white hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
