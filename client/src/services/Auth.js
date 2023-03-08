b55e4aa854937e32d059d1beab33a0b3bb1bc554
jascolil01 <jayasco246@gmail.com>
2023-03-07 21:19:41 -0500

    acquired necessary info for functions

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -5,8 +5,12 @@ export const SignInUser = async (data) => {
     const res = await Client.post('auth/login', data)
     // Set the current signed in users token to localStorage
     localStorage.setItem('token', res.data.token)
-    return res.data.data
+    const userInfo = await Client.get(`user/get_user/by_password/${res.data.data.password}`)
+    localStorage.setItem('userId', userInfo.data.id)
+    localStorage.setItem('name', userInfo.data.name)
+    localStorage.setItem('userName', userInfo.data.userName)
 
+    return userInfo
   } catch (error) {
     throw error
   }
@@ -30,3 +34,4 @@ export const CheckSession = async () => {
     throw error
   }
 }
+
