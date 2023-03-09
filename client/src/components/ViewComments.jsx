bcfae25766f1a9eb32714625e62be52480818487
NellyNel520 <channelharris520@gmail.com>
2023-03-09 02:07:39 -0500

    fixed error

--- client/src/components/ViewComments.jsx
+++ client/src/components/ViewComments.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { Link, useParams } from 'react-router-dom'
@@ -8,15 +7,15 @@ const ViewComments = () => {
   const [comments, setComments] = useState([])
   let { id } = useParams()
   console.log(id)
-  
+
   useEffect(() => {
-    
+
 
   const handleComments = async () => {
     const res = await Client.get(`/comment/find_comments/by_postId/${id}`)
     console.log(res.data)
     setComments(res.data)
-    
+
   }
   handleComments()
 }, [id])
@@ -26,7 +25,7 @@ const ViewComments = () => {
   return comments ? (
   <div className='h-screen pt-10'>
     <div className='grid justify-center h-auto'>
-    
+
       <div className='mt-5 md:col-span-2  rounded  border w-full md:mx-0 lg:mx-0 bg-white px-[2rem] py-[2rem]'>
       <div className='flex'>
         <Link to={'/feed'}><AiOutlineArrowLeft size={25} className='hover:text-orange-500 flex' /></Link>
@@ -51,19 +50,16 @@ const ViewComments = () => {
         {/* <div className='flex border-b-2 mb-2 py-2'>
             <img className="w-10 h-10 mr-2 object-cover rounded-full" src="https://img.freepik.com/free-icon/user_318-159711.jpg?size=338&ext=jpg" alt='profile-pic'/>
             <p><span className='font-medium mr-2 pl-2'>NellyNel520</span>Section where 1st two comments are being shown replace with comment username and comment content</p>
-
         </div> */}
 
 
-        
+
 
       </div>
+
 
     </div>
   </div>
   ) : null
 }
-
 export default ViewComments
-
-
