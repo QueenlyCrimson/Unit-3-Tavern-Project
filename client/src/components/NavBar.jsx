6368da7a504f3ad11c09617d0a41e3f7715fb55e
NellyNel520 <channelharris520@gmail.com>
2023-03-10 01:22:05 -0500

    additional styling and changed public nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -36,9 +36,9 @@ const NavBar = ({ user, handleLogOut }) => {
           <li className='p-4 hover:text-orange-500'><Link to="/makeProfile">Register</Link></li>
           <SignInButton user={user} handleLogOut={handleLogOut} />
           <li className='p-4 hover:text-orange-500'><Link to="/about">About</Link></li>
-          <li className='p-4 hover:text-orange-500'><Link to="/makePost">MakePost</Link></li>
+          {/* <li className='p-4 hover:text-orange-500'><Link to="/makePost">MakePost</Link></li>
           <li className='p-4 hover:text-orange-500'><Link to="/updateProfile">Update Profile</Link></li>
-          <li className='p-4 hover:text-orange-500'><Link to="/profile">ProfilePage</Link></li>
+          <li className='p-4 hover:text-orange-500'><Link to="/profile">ProfilePage</Link></li> */}
         </ul>
       </div>
     </nav>
