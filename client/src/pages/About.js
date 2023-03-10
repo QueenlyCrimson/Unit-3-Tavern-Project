a73826b6824d680ed2c0ab6b11eae813c750d854
NellyNel520 <channelharris520@gmail.com>
2023-03-10 00:37:14 -0500

    styled about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,7 @@ import Description from '../components/Description'
 const About = () => {
   return (
     <div className='text-white'>
-      <h1 className='w-full text-4xl font-bold text-center pt-4 font-Crisis'>About Us</h1>
+      <h1 className='w-full text-5xl font-bold text-center pt-4 font-Playfair'>About Us</h1>
       <Description />
       <AboutCards />
 
